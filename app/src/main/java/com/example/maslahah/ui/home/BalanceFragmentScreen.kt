712470d857e4a02cedd14c9127f27bca33e1d132
mohamed <ma2078941@gmail.com>
2021-11-01 16:26:47 +0200

    update balance value

--- app/src/main/java/com/example/maslahah/ui/home/BalanceFragmentScreen.kt
+++ app/src/main/java/com/example/maslahah/ui/home/BalanceFragmentScreen.kt
@@ -6,13 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.maslahah.R
+import com.example.maslahah.databinding.FragmentBalanceScreenBinding
+import com.example.maslahah.databinding.FragmentTaxScreenBinding
 
 
 class BalanceFragmentScreen : Fragment() {
 
 
+    private var _binding: FragmentBalanceScreenBinding? = null
+    private val binding get() = _binding!!
+
+    var value = 0.0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        value = BalanceFragmentScreenArgs.fromBundle(requireArguments()).value
 
     }
 
@@ -22,6 +30,13 @@ class BalanceFragmentScreen : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_balance_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentBalanceScreenBinding.bind(view)
+
+        binding.balanceValue.text = value.toString()
     }
 
 }
